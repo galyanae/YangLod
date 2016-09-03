621ed58ca690590e4365149abfe3a75c2c4ea6d9
Alexey Soshin <alexey.soshin@gettaxi.com>
2016-09-03 13:25:34 +0300

    Complete sending to FCM
    Key is still wrong

--- app/src/main/java/com/goodthinking/younglod/user/NewMessageToUsers.java
+++ app/src/main/java/com/goodthinking/younglod/user/NewMessageToUsers.java
@@ -1,8 +1,10 @@
 package com.goodthinking.younglod.user;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
@@ -16,10 +18,29 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class NewMessageToUsers extends AppCompatActivity {
+
+
+    //TODO: This API key is wrong, need to get another from 'Manage -> Cloud Messaging'
+    private static final String API_KEY = "AIzaSyB7ajsBlHR8vSzuR3shPCA4g9sFzxnqw6k";
+    private Map<String, String> events = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,24 +58,27 @@ public class NewMessageToUsers extends AppCompatActivity {
     private void populateSpinner() {
         DatabaseReference db = FirebaseDatabase.getInstance().getReference();
 
-        final List<String> events = new ArrayList<>();
+        events = new HashMap<>();
         db.child("Tables").child("Events").addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot snapshot) {
                 for (DataSnapshot child: snapshot.getChildren()) {
                     if (child != null && child.child("eventName") != null) {
                         String eventName = (String) child.child("eventName").getValue();
-                        events.add(eventName);
+                        String eventId =  child.getKey();
+                        events.put(eventName, eventId);
                     }
 
                 }
                 ArrayAdapter<String> adapter = new ArrayAdapter<>(
                         getApplicationContext(),
                         android.R.layout.simple_list_item_single_choice,
-                        events);
+                        new ArrayList<>(events.keySet()));
 
                 Spinner spinner = (Spinner) findViewById(R.id.eventsSpinner);
                 spinner.setAdapter(adapter);
+
+                setOnItemSelected(spinner);
             }
 
             @Override
@@ -66,18 +90,94 @@ public class NewMessageToUsers extends AppCompatActivity {
         });
     }
 
+    private void setOnItemSelected(Spinner spinner) {
+        spinner.setOnItemSelectedListener(new EventsSpinnerOnItemSelectedListener());
+    }
+
     private class SendMessageToUsersClickListener implements View.OnClickListener {
         @Override
         public void onClick(View v) {
             TextView sendTextView = (TextView) findViewById(R.id.sendText);
 
-            String textToSend = sendTextView.getText().toString().trim();
+            final String textToSend = sendTextView.getText().toString().trim();
 
             if (textToSend.length() > 0) {
-
+                AsyncTask.execute(new Runnable() {
+                    @Override
+                    public void run() {
+                        sendToTopic(textToSend);
+                    }
+                });
             }
 
         }
+
+        private void sendToTopic(String textToSend) {
+            try {
+                URL url = new URL("https://fcm.googleapis.com/fcm/send");
+                HttpURLConnection conn = null;
+                try {
+                    conn = (HttpURLConnection)  url.openConnection();
+                    send(textToSend, conn);
+
+                    int responseCode = conn.getResponseCode();
+
+                    System.out.println("FCM Response " + responseCode);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                } finally {
+                    if (conn != null) {
+                        conn.disconnect();
+                    }
+                }
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        private void send(String textToSend, HttpURLConnection conn) throws IOException, JSONException {
+            conn.setRequestProperty("Content-Type",
+                    "application/json");
+            conn.setRequestProperty("Authorization", "key="+API_KEY);
+
+            conn.setRequestMethod("POST");
+
+            DataOutputStream wr = new DataOutputStream (
+                    conn.getOutputStream ());
+            wr.writeBytes (getPayload(selectedItemId, textToSend));
+            wr.flush ();
+            wr.close ();
+        }
+
+        private String getPayload(String selectedItemId, String textToSend) throws JSONException {
+
+            JSONObject data = new JSONObject();
+
+            data.put("message", textToSend);
+
+            JSONObject payload = new JSONObject();
+
+            payload.put("to", "/topics/"+selectedItemId);
+            payload.put("data", data);
+
+            return payload.toString();
+        }
     }
 
+    private String selectedItemId;
+
+    private class EventsSpinnerOnItemSelectedListener implements android.widget.AdapterView.OnItemSelectedListener {
+        @Override
+        public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+            String selectedItem = parent.getItemAtPosition(position).toString();
+
+            selectedItemId = events.get(selectedItem);
+        }
+
+        @Override
+        public void onNothingSelected(AdapterView<?> parent) {
+
+        }
+    }
 }
